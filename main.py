9b4963bca2f9804b0f0fe4e64bfeb0a6439a585a
dnzbk <denis@nzbget.com>
2025-05-23 16:10:37 +0300

    Release v3.1

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # EasySort post-processing script for NZBGet.
 #
 # Copyright (C) 2015 Andrey Prygunkov <hugbug@users.sourceforge.net>
+# Copyright (C) 2024-2025 Denis <denis@nzbget.com>
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU Lesser General Public License as published by
@@ -25,68 +26,94 @@ import shutil
 import traceback
 
 # Exit codes used by NZBGet
-POSTPROCESS_SUCCESS=93
-POSTPROCESS_NONE=95
-POSTPROCESS_ERROR=94
+POSTPROCESS_SUCCESS = 93
+POSTPROCESS_NONE = 95
+POSTPROCESS_ERROR = 94
 
 # Check if the script is called from nzbget 15.0 or later
-if not 'NZBOP_NZBLOG' in os.environ:
-    print('*** NZBGet post-processing script ***')
-    print('This script is supposed to be called from nzbget (15.0 or later).')
+if not "NZBOP_NZBLOG" in os.environ:
+    print("*** NZBGet post-processing script ***")
+    print("This script is supposed to be called from nzbget (15.0 or later).")
     sys.exit(POSTPROCESS_ERROR)
 
 # Check if directory still exist (for post-process again)
-if not os.path.exists(os.environ['NZBPP_DIRECTORY']):
-    print('[INFO] Destination directory %s doesn\'t exist, exiting' % os.environ['NZBPP_DIRECTORY'])
+if not os.path.exists(os.environ["NZBPP_DIRECTORY"]):
+    print(
+        "[INFO] Destination directory %s doesn't exist, exiting"
+        % os.environ["NZBPP_DIRECTORY"]
+    )
     sys.exit(POSTPROCESS_NONE)
 
 # Check status for errors
-if os.environ['NZBPP_TOTALSTATUS'] != 'SUCCESS':
-    print('[WARNING] Download of "%s" has failed, exiting' % (os.environ['NZBPP_NZBNAME']))
+if os.environ["NZBPP_TOTALSTATUS"] != "SUCCESS":
+    print(
+        '[WARNING] Download of "%s" has failed, exiting' % (os.environ["NZBPP_NZBNAME"])
+    )
     sys.exit(POSTPROCESS_NONE)
 
 # Check if all required script config options are present in config file
-required_options = ('NZBPO_DestDir', 'NZBPO_UseCategoryDir', 'NZBPO_UseNzbParentDir', 'NZBPO_Extensions', 'NZBPO_MinSize',
-    'NZBPO_Overwrite', 'NZBPO_Cleanup', 'NZBPO_Preview', 'NZBPO_Verbose')
+required_options = (
+    "NZBPP_NzbName",
+    "NZBPO_DestDir",
+    "NZBPO_UseCategoryDir",
+    "NZBPO_UseNzbParentDir",
+    "NZBPO_Extensions",
+    "NZBPO_MinSize",
+    "NZBPO_Overwrite",
+    "NZBPO_Cleanup",
+    "NZBPO_Preview",
+    "NZBPO_Verbose",
+    "NZBPP_Category",
+)
 for optname in required_options:
-    if (not optname.upper() in os.environ):
-        print('[ERROR] Option %s is missing in configuration file. Please check script settings' % optname[6:])
+    if not optname.upper() in os.environ:
+        print(
+            "[ERROR] Option %s is missing in configuration file. Please check script settings"
+            % optname[6:]
+        )
         sys.exit(POSTPROCESS_ERROR)
 
 # Init script config options
-nzb_name=os.environ['NZBPP_NZBNAME']
-download_dir=os.environ['NZBPP_DIRECTORY']
-dest_dir=os.environ['NZBPO_DESTDIR']
-useCategoryDir=os.environ['NZBPO_UseCategoryDir'] == 'yes'
-useNzbParentDir=os.environ['NZBPO_UseNzbParentDir'] == 'yes'
-extensions=os.environ['NZBPO_EXTENSIONS'].lower().split(',')
-min_size=int(os.environ['NZBPO_MINSIZE'])
+nzb_name = os.environ["NZBPP_NZBNAME"]
+download_dir = os.environ["NZBPP_DIRECTORY"]
+dest_dir = os.environ["NZBPO_DESTDIR"]
+use_category_dir = os.environ["NZBPO_USECATEGORYDIR"] == "yes"
+use_nzb_parent_dir = os.environ["NZBPO_USENZBPARENTDIR"] == "yes"
+extensions = os.environ["NZBPO_EXTENSIONS"].lower().split(",")
+min_size = int(os.environ["NZBPO_MINSIZE"])
 min_size <<= 10
-overwrite=os.environ['NZBPO_OVERWRITE'] == 'yes'
-cleanup=os.environ['NZBPO_CLEANUP'] == 'yes'
-preview=os.environ['NZBPO_PREVIEW'] == 'yes'
-verbose=os.environ['NZBPO_VERBOSE'] == 'yes'
+overwrite = os.environ["NZBPO_OVERWRITE"] == "yes"
+cleanup = os.environ["NZBPO_CLEANUP"] == "yes"
+preview = os.environ["NZBPO_PREVIEW"] == "yes"
+verbose = os.environ["NZBPO_VERBOSE"] == "yes"
+category = os.environ["NZBPP_CATEGORY"]
 
-if dest_dir == '':
-    print('[WARNING] Option DestDir cannot be empty, exiting')
+if dest_dir == "":
+    print("[WARNING] Option DestDir cannot be empty, exiting")
     sys.exit(POSTPROCESS_ERROR)
 
 # Relative paths
-if not (dest_dir[1] == '/' or dest_dir[1] == '\\' or (len(dest_dir) > 2) and dest_dir[2] == ':'):
-    dest_dir = os.path.join(download_dir, dest_dir) 
+if not (
+    dest_dir[1] == "/"
+    or dest_dir[1] == "\\"
+    or (len(dest_dir) > 2)
+    and dest_dir[2] == ":"
+):
+    dest_dir = os.path.join(download_dir, dest_dir)
+
+if use_category_dir:
+    dest_dir = os.path.join(dest_dir, category)
+
+if use_nzb_parent_dir:
+    dest_dir = os.path.join(dest_dir, nzb_name)
+
 dest_dir = os.path.abspath(dest_dir)
 
-if useCategoryDir:
-    dest_dir = os.path.join(dest_dir, os.environ['NZBPP_CATEGORY'])
-
-if useNzbParentDir:
-    dest_dir = os.path.join(dest_dir, os.environ['NZBPP_NZBNAME'])
-
 if verbose:
-    print('Normalized dest directory: %s' % dest_dir)
+    print("Normalized dest directory: %s" % dest_dir)
 
 if preview:
-    print('[WARNING] *** PREVIEW MODE ON - NO CHANGES TO FILE SYSTEM ***')
+    print("[WARNING] *** PREVIEW MODE ON - NO CHANGES TO FILE SYSTEM ***")
 
 # List of moved files (source path)
 moved_src_files = []
@@ -96,55 +123,58 @@ moved_dst_files = []
 
 # Separator character used between file name and opening brace
 # for duplicate files such as "My Movie (2).mkv"
-dupe_separator = ' '
+dupe_separator = " "
+
 
 def guess_dupe_separator(filename):
-    """ Find out a char most suitable as dupe_separator
-    """ 
+    """Find out a char most suitable as dupe_separator"""
     global dupe_separator
 
-    dupe_separator = ' '
+    dupe_separator = " "
     fname = os.path.splitext(filename)[0]
 
-    if (fname.find('.') > -1):
-        dupe_separator = '.'
+    if fname.find(".") > -1:
+        dupe_separator = "."
         return
 
-    if (fname.find('_') > -1):
-        dupe_separator = '_'
+    if fname.find("_") > -1:
+        dupe_separator = "_"
         return
+
 
 def unique_name(new):
-    """ Adds unique numeric suffix to destination file name to avoid overwriting
-        such as "filename.(2).ext", "filename.(3).ext", etc.
-        If existing file was created by the script it is renamed to "filename.(1).ext".
+    """Adds unique numeric suffix to destination file name to avoid overwriting
+    such as "filename.(2).ext", "filename.(3).ext", etc.
+    If existing file was created by the script it is renamed to "filename.(1).ext".
     """
     fname, fext = os.path.splitext(new)
     suffix_num = 2
     while True:
-        new_name = fname + dupe_separator + '(' + str(suffix_num) + ')' + fext
+        new_name = fname + dupe_separator + "(" + str(suffix_num) + ")" + fext
         if not os.path.exists(new_name) and new_name not in moved_dst_files:
             break
         suffix_num += 1
     return new_name
 
+
 def optimized_move(old, new):
     try:
         os.rename(old, new)
     except OSError as ex:
-        print('[DETAIL] Rename failed ({}), performing copy: {}'.format(ex, new))
+        print("[DETAIL] Rename failed ({}), performing copy: {}".format(ex, new))
         shutil.copyfile(old, new)
         os.remove(old)
 
+
 def rename(old, new):
-    """ Moves the file to its sorted location.
-        It creates any necessary directories to place the new file and moves it.
+    """Moves the file to its sorted location.
+    It creates any necessary directories to place the new file and moves it.
     """
     if os.path.exists(new) or new in moved_dst_files:
         if overwrite and new not in moved_dst_files:
             os.remove(new)
             optimized_move(old, new)
-            print('[INFO] Overwrote: %s' % new)
+            print("[INFO] Overwrote: %s" % new)
         else:
             # rename to filename.(2).ext, filename.(3).ext, etc.
             new = unique_name(new)
@@ -154,29 +184,31 @@ def rename(old, new):
             if not os.path.exists(os.path.dirname(new)):
                 os.makedirs(os.path.dirname(new))
             optimized_move(old, new)
-        print('[INFO] Moved: %s' % new)
+        print("[INFO] Moved: %s" % new)
     moved_src_files.append(old)
     moved_dst_files.append(new)
     return new
 
+
 def cleanup_download_dir():
     if verbose:
-        print('Cleanup')
+        print("Cleanup")
 
-    # Now delete all files with nice logging                
+    # Now delete all files with nice logging
     for root, dirs, files in os.walk(download_dir):
         for filename in files:
             path = os.path.join(root, filename)
             if not preview or path not in moved_src_files:
                 if not preview:
                     os.remove(path)
-                print('[INFO] Deleted: %s' % path)
+                print("[INFO] Deleted: %s" % path)
     if not preview:
         shutil.rmtree(download_dir)
-    print('[INFO] Deleted: %s' % download_dir)
+    print("[INFO] Deleted: %s" % download_dir)
+
 
 def construct_path(filename):
-    """ Parses the filename and generates new name for renaming """
+    """Parses the filename and generates new name for renaming"""
 
     if verbose:
         print("filename: %s" % filename)
@@ -192,9 +224,10 @@ def construct_path(filename):
     new_path = os.path.join(dest_dir, filename)
 
     if verbose:
-        print('destination path: %s' % new_path)
-        
+        print("destination path: %s" % new_path)
+
     return new_path
+
 
 # Flag indicating that anything was moved. Cleanup possible.
 files_moved = False
@@ -209,18 +242,19 @@ for root, dirs, files in os.walk(download_dir):
     for old_filename in files:
         try:
             if verbose:
-                print('[INFO] Processing: %s' % old_filename)
+                print("[INFO] Processing: %s" % old_filename)
 
             old_path = os.path.join(root, old_filename)
 
             # Check extension
-            if extensions != ['']:
+            if extensions != [""]:
                 ext = os.path.splitext(old_filename)[1].lower()
-                if ext not in extensions: continue
+                if ext not in extensions:
+                    continue
 
             # Check minimum file size
             if os.path.getsize(old_path) < min_size:
-                print('[INFO] Skipping small: %s' % old_filename)
+                print("[INFO] Skipping small: %s" % old_filename)
                 continue
 
             # This is our file, we should process it
@@ -228,12 +262,12 @@ for root, dirs, files in os.walk(download_dir):
 
         except Exception as e:
             errors = True
-            print('[ERROR] Failed: %s' % old_filename)
-            print('[ERROR] %s' % e)
+            print("[ERROR] Failed: %s" % old_filename)
+            print("[ERROR] %s" % e)
             traceback.print_exc()
 
 if verbose:
-    print('File list: %s' % move_files)
+    print("File list: %s" % move_files)
 
 for old_path in move_files:
     try:
@@ -246,22 +280,22 @@ for old_path in move_files:
 
     except Exception as e:
         errors = True
-        print('[ERROR] Failed: %s' % old_filename)
-        print('[ERROR] %s' % e)
+        print("[ERROR] Failed: %s" % old_filename)
+        print("[ERROR] %s" % e)
         traceback.print_exc()
 
 # Inform NZBGet about new destination path
-finaldir = ''
+finaldir = ""
 uniquedirs = []
 for filename in moved_dst_files:
     dir = os.path.dirname(filename)
     if dir not in uniquedirs:
         uniquedirs.append(dir)
-        finaldir += '|' if finaldir != '' else ''
+        finaldir += "|" if finaldir != "" else ""
         finaldir += dir
 
-if finaldir != '':
-    print('[NZB] FINALDIR=%s' % finaldir)
+if finaldir != "":
+    print("[NZB] FINALDIR=%s" % finaldir)
 
 # Cleanup if:
 # 1) files were moved AND
--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Copyright (C) 2024 Denis <denis@nzbget.com>
+# Copyright (C) 2024-2025 Denis <denis@nzbget.com>
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU Lesser General Public License as published by
@@ -13,7 +13,7 @@
 # GNU Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with the program.  If not, see <http://www.gnu.org/licenses/>.
+# along with the program.  If not, see <https://www.gnu.org/licenses/>.
 #
 
 
@@ -25,87 +25,128 @@ import unittest
 import shutil
 import json
 
-POSTPROCESS_SUCCESS=93
-POSTPROCESS_NONE=95
-POSTPROCESS_ERROR=94
+POSTPROCESS_SUCCESS = 93
+POSTPROCESS_NONE = 95
+POSTPROCESS_ERROR = 94
 
 root = dirname(__file__)
-test_data_dir = root + '/test_data/'
-tmp_dir = root + '/tmp/'
-dst_dir = root + '/tmp/dst/'
-dwn_dir = root + '/tmp/dwn/'
-test_file = 'movie.mp4'
+test_data_dir = root + "/test_data/"
+tmp_dir = root + "/tmp/"
+dst_dir = root + "/tmp/dst/"
+dwn_dir = root + "/tmp/dwn/"
+test_file = "movie.mp4"
+nzb_name = "TestNZB"
+category = "Movies"
 
-host = '127.0.0.1'
-username = 'TestUser'
-password = 'TestPassword'
-port = '6789'
+host = "127.0.0.1"
+username = "TestUser"
+password = "TestPassword"
+port = "6789"
 
-def get_python(): 
-	if os.name == 'nt':
-		return 'python'
-	return 'python3'
+
+def get_python():
+    if os.name == "nt":
+        return "python"
+    return "python3"
+
 
 def run_script():
-	sys.stdout.flush()
-	proc = subprocess.Popen([get_python(), root + '/main.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=os.environ.copy())
-	out, err = proc.communicate()
-	proc.pid
-	ret_code = proc.returncode
-	return (out.decode(), int(ret_code), err.decode())
+    sys.stdout.flush()
+    proc = subprocess.Popen(
+        [get_python(), root + "/main.py"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        env=os.environ.copy(),
+    )
+    out, err = proc.communicate()
+    proc.pid
+    ret_code = proc.returncode
+    return (out.decode(), int(ret_code), err.decode())
+
 
 def set_defaults_env():
-	# NZBGet global options
-	os.environ['NZBOP_CONTROLPORT'] = port
-	os.environ['NZBOP_CONTROLIP'] = host
-	os.environ['NZBOP_CONTROLUSERNAME'] = username
-	os.environ['NZBOP_CONTROLPASSWORD'] = password
-	os.environ['NZBOP_NZBLOG'] = tmp_dir + '/log.txt'
-	os.environ['NZBPP_TOTALSTATUS'] = 'SUCCESS'
-	os.environ['NZBPO_PASSACTION'] = 'PASSACTION'
+    # NZBGet global options
+    os.environ["NZBOP_CONTROLPORT"] = port
+    os.environ["NZBOP_CONTROLIP"] = host
+    os.environ["NZBOP_CONTROLUSERNAME"] = username
+    os.environ["NZBOP_CONTROLPASSWORD"] = password
+    os.environ["NZBOP_NZBLOG"] = tmp_dir + "/log.txt"
+    os.environ["NZBPP_TOTALSTATUS"] = "SUCCESS"
+    os.environ["NZBPO_PASSACTION"] = "PASSACTION"
+    os.environ["NZBPP_CATEGORY"] = ""
 
-	# script options
-	os.environ['NZBPO_OVERWRITE'] = 'yes'
-	os.environ['NZBPO_CLEANUP'] = 'yes'
-	os.environ['NZBPO_PREVIEW'] = 'no'
-	os.environ['NZBPO_VERBOSE'] = 'no'
-	os.environ['NZBPP_TOTALSTATUS'] = 'SUCCESS'
-	os.environ['NZBPP_DIRECTORY'] = dwn_dir
-	os.environ['NZBNA_NZBNAME'] = 'TestNZB'
-	os.environ['NZBOP_TEMPDIR'] = tmp_dir
-	os.environ['NZBPO_DESTDIR'] = dst_dir
-	os.environ['NZBPO_EXTENSIONS'] = '.mp4'
-	os.environ['NZBPP_NZBNAME'] = test_file
+    # script options
+    os.environ["NZBPO_OVERWRITE"] = "yes"
+    os.environ["NZBPO_CLEANUP"] = "yes"
+    os.environ["NZBPO_PREVIEW"] = "no"
+    os.environ["NZBPO_VERBOSE"] = "no"
+    os.environ["NZBPP_TOTALSTATUS"] = "SUCCESS"
+    os.environ["NZBPP_DIRECTORY"] = dwn_dir
+    os.environ["NZBPP_NZBNAME"] = nzb_name
+    os.environ["NZBOP_TEMPDIR"] = tmp_dir
+    os.environ["NZBPO_DESTDIR"] = dst_dir
+    os.environ["NZBPO_EXTENSIONS"] = ".mp4"
+    os.environ["NZBPO_USECATEGORYDIR"] = "no"
+    os.environ["NZBPO_USENZBPARENTDIR"] = "no"
+    os.environ["NZBPO_MINSIZE"] = "0"
+
 
 class Tests(unittest.TestCase):
 
-	def test_do_nothing_if_file_too_small(self):
-		os.mkdir(tmp_dir)
-		os.mkdir(dwn_dir)
-		shutil.copyfile(test_data_dir + test_file, dwn_dir + test_file)
-		set_defaults_env()
-		os.environ['NZBPO_MINSIZE'] = '1000'
-		[_, code, _] = run_script()
-		shutil.rmtree(tmp_dir)
-		self.assertEqual(code, POSTPROCESS_NONE)
+    def test_do_nothing_if_file_too_small(self):
+        os.mkdir(tmp_dir)
+        os.mkdir(dwn_dir)
+        shutil.copyfile(test_data_dir + test_file, dwn_dir + test_file)
+        set_defaults_env()
+        os.environ["NZBPO_MINSIZE"] = "1000"
+        [_, code, _] = run_script()
+        shutil.rmtree(tmp_dir)
+        self.assertEqual(code, POSTPROCESS_NONE)
 
-	def test_move_file(self):
-		os.mkdir(tmp_dir)
-		os.mkdir(dwn_dir)
-		shutil.copyfile(test_data_dir + test_file, dwn_dir + test_file)
-		set_defaults_env()
-		os.environ['NZBPO_MINSIZE'] = '0'
-		[_, code, _] = run_script()
-		shutil.rmtree(tmp_dir)
-		self.assertEqual(code, POSTPROCESS_SUCCESS)
+    def test_move_file(self):
+        os.mkdir(tmp_dir)
+        os.mkdir(dwn_dir)
+        shutil.copyfile(test_data_dir + test_file, dwn_dir + test_file)
+        set_defaults_env()
+        [_, code, _] = run_script()
+        self.assertTrue(os.path.exists(dst_dir + test_file))
+        shutil.rmtree(tmp_dir)
+        self.assertEqual(code, POSTPROCESS_SUCCESS)
 
-	def test_manifest(self):
-		with open(root + '/manifest.json', encoding='utf-8') as file:
-			try:
-				json.loads(file.read())
-			except ValueError as e:
-				self.fail('manifest.json is not valid.')
+    def test_move_file_with_category(self):
+        os.mkdir(tmp_dir)
+        os.mkdir(dwn_dir)
+        shutil.copyfile(test_data_dir + test_file, dwn_dir + test_file)
+        set_defaults_env()
+        os.environ["NZBPP_CATEGORY"] = category
+        os.environ["NZBPO_USECATEGORYDIR"] = "yes"
+        [_, code, _] = run_script()
+        self.assertTrue(os.path.exists(dst_dir + category + "/" + test_file))
+        shutil.rmtree(tmp_dir)
+        self.assertEqual(code, POSTPROCESS_SUCCESS)
+
+    def test_move_file_with_category_and_nzb_parent(self):
+        os.mkdir(tmp_dir)
+        os.mkdir(dwn_dir)
+        shutil.copyfile(test_data_dir + test_file, dwn_dir + test_file)
+        set_defaults_env()
+        os.environ["NZBPP_CATEGORY"] = category
+        os.environ["NZBPO_USENZBPARENTDIR"] = "yes"
+        os.environ["NZBPO_USECATEGORYDIR"] = "yes"
+        [_, code, _] = run_script()
+        self.assertTrue(
+            os.path.exists(dst_dir + "/" + category + "/" + nzb_name + "/" + test_file)
+        )
+        shutil.rmtree(tmp_dir)
+        self.assertEqual(code, POSTPROCESS_SUCCESS)
+
+    def test_manifest(self):
+        with open(root + "/manifest.json", encoding="utf-8") as file:
+            try:
+                json.loads(file.read())
+            except ValueError as e:
+                self.fail("manifest.json is not valid.")
 
 
-if __name__ == '__main__':
-	unittest.main()
+if __name__ == "__main__":
+    unittest.main()
